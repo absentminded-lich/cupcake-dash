a5e9cf49226ff772d63fe1a985f2956db1d2e0f7
Brett Parker <absentminded.lich@gmail.com>
2023-02-26 13:48:57 -0500

    Fix movement when holding key

--- player.gd
+++ player.gd
@@ -1,23 +1,32 @@
 extends CharacterBody2D
 
 const GRID_SNAP := 64
-const SPEED := 0.25
+const SPEED := 0.35
 
-func _input(event) -> void:
-	if event.is_action_pressed("ui_down"):
+var is_moving := false
+
+func _input(_Input) -> void:
+	if Input.is_action_pressed("ui_down"):
 		_move(Vector2.DOWN)
-	if event.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		_move(Vector2.LEFT)
-	if event.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		_move(Vector2.RIGHT)
-	if event.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		_move(Vector2.UP)
 		
 func _move(dir: Vector2) -> void:
 	_move_tween(dir)
 
 func _move_tween(dir: Vector2) -> void:
-	var tween: Tween = get_tree().create_tween()
+	if (is_moving):
+		return
+		
+	is_moving = true
+	var tween : Tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "global_position", global_position + dir * GRID_SNAP, SPEED)
+	await tween.finished
+	
+	is_moving = false
